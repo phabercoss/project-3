efdd0a267ac89785adfecc71c64dd50ff4952aca
Phallyn <phabercoss@hawk.iit.edu>
2019-04-17 01:55:54 -0500

    Added google map starting and media queries

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function initMap() {
-var latlng = {lat: 41.869187, lng: -87.66584},
-var map = new google.maps.Map(document.getElementById('map'), {center: latlng, zoom: 6});
+var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 41.869187, lng: -87.66584},
+      zoom: 18
+  });
 }
