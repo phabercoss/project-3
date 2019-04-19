dc8ba8bc72fe5a3227a52fd679bfe2829fd95b50
Phallyn <phabercoss@hawk.iit.edu>
2019-04-19 00:26:33 -0500

    Added new mobile queries

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function initMap() {
 var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 41.869187, lng: -87.66584},
+      center: {lat: 41.8691, lng: -87.66584},
       zoom: 18
   });
 }
