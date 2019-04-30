26425d06ec2628c0fac096e4addfc687feccbbf8
Phallyn <phabercoss@hawk.iit.edu>
2019-04-30 10:28:11 -0500

    Tried adding marker but doesnt work

--- js/script.js
+++ js/script.js
@@ -3,4 +3,15 @@ var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 41.8691, lng: -87.66584},
       zoom: 18
   });
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  var marker = new google.maps.Marker({
+    map: map,
+    title: 'Pomepii and Surrounding area'
+  });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
 }
