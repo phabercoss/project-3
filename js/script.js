a6404da2497be44f5b41a797fa2a489d136dd67c
Phallyn <phabercoss@hawk.iit.edu>
2019-04-30 20:08:46 -0500

    Added a marker

--- js/script.js
+++ js/script.js
@@ -3,15 +3,10 @@ var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 41.8691, lng: -87.66584},
       zoom: 18
   });
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-  });
-
-  var marker = new google.maps.Marker({
-    map: map,
-    title: 'Pomepii and Surrounding area'
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
+  var pompeiiPosition= {lat: 41.869183,lng: -87.66584};
+  	var marker = new google.maps.Marker({
+  			position: pompeiiPosition,
+  			map: map,
+  			title: 'Pompeii'
+  			});
 }
